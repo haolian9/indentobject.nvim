7abfca930d0867bd495f0b7f5582e168c49c36ac
haoliang <haoliang0.1.2@gmail.com>
2023-06-15 11:18:01 +0800

    initial commit

---
+++ lua/indentobject.lua
@@ -0,0 +1,50 @@
+-- design choices
+-- * not work on top level
+-- * ignore indent=0 when searching next/prev line
+-- * ignore blank lines in the top and bottom of the range
+
+local cthulhu = require("cthulhu")
+local vsel = require("infra.vsel")
+local jelly = require("infra.jellyfish")("indentobject", vim.log.levels.DEBUG)
+local nvimkeys = require("infra.nvimkeys")
+
+local api = vim.api
+
+return function()
+  local winid = api.nvim_get_current_win()
+  local bufnr = api.nvim_win_get_buf(winid)
+  local curow = api.nvim_win_get_cursor(winid)[1]
+  local nsp = vim.fn.indent(curow)
+  if nsp == 0 then error("refuse to work on top indent level") end
+
+  local maxrow = api.nvim_buf_line_count(bufnr)
+
+  local toprow
+  for i = curow, 1, -1 do
+    local insp = vim.fn.indent(i)
+    if insp == 0 then
+      if not cthulhu.nvim.is_empty_line(bufnr, i - 1) then break end
+      -- skip blank line
+    elseif insp < nsp then
+      break
+    else
+      toprow = i
+    end
+  end
+
+  local botrow
+  for i = curow, maxrow do
+    local insp = vim.fn.indent(i)
+    if insp == 0 then
+      if not cthulhu.nvim.is_empty_line(bufnr, i - 1) then break end
+      -- skip blank line
+    elseif insp < nsp then
+      break
+    else
+      botrow = i
+    end
+  end
+
+  jelly.debug("curow=%d, nsp=%d, rowrange=%d-%d", curow, nsp, toprow, botrow)
+  vsel.select_lines(toprow - 1, botrow - 1 + 1)
+end
---
+++ readme.md
@@ -0,0 +1,27 @@
+providing an indent object for neovim
+
+## design choices
+* not work on top level
+* ignore indent=0 when searching next/prev line
+* ignore blank lines in the top and bottom of the range
+
+## status
+* it just works (tm)
+* it is feature-freezed
+
+## prerequisites
+* nvim 0.9.*
+* haolian9/cthulhu
+* haolian9/infra.nvim
+
+## usage
+
+my personal keymaps
+
+```
+m.v("ii", function() require("indentobject")() end)
+```
+
+## thanks
+
+[vim-indent-object](https://github.com/michaeljsmith/vim-indent-object) was my friend for a long time, and this plugin of mine is inspired by it.
