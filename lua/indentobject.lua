9a9eef450a680b6c3a5c8bdc96da82a7114ac1d6
haoliang <haoliang0.1.2@gmail.com>
2024-07-18 19:20:27 +0800

    sync with upstream

--- lua/indentobject.lua
+++ lua/indentobject.lua
@@ -4,48 +4,46 @@
 -- * ignore blank lines in the top and bottom of the range
 
 local cthulhu = require("cthulhu")
-local vsel = require("infra.vsel")
-local jelly = require("infra.jellyfish")("indentobject", vim.log.levels.DEBUG)
+local buflines = require("infra.buflines")
+local itertools = require("infra.itertools")
+local jelly = require("infra.jellyfish")("indentobject")
 local jumplist = require("infra.jumplist")
+local ni = require("infra.ni")
+local vsel = require("infra.vsel")
+local wincursor = require("infra.wincursor")
 
-local api = vim.api
+---@param bufnr integer
+---@param nsp integer
+---@param range fun():integer? @range of rows
+---@return integer @row
+local function resolve_stop_row(bufnr, nsp, range)
+  local row
+  for i in range do
+    local insp = vim.fn.indent(i)
+    if insp == 0 then
+      if not cthulhu.nvim.is_empty_line(bufnr, i - 1) then break end
+      -- skip blank line
+    elseif insp < nsp then
+      break
+    else
+      row = i
+    end
+  end
+  assert(row ~= nil)
+  return row
+end
 
 return function()
-  local winid = api.nvim_get_current_win()
-  local bufnr = api.nvim_win_get_buf(winid)
-  local curow = api.nvim_win_get_cursor(winid)[1]
+  local winid = ni.get_current_win()
+  local bufnr = ni.win_get_buf(winid)
+  local curow = wincursor.row(winid)
   local nsp = vim.fn.indent(curow)
   if nsp == 0 then return jelly.err("refuse to work on top indent level") end
 
-  local maxrow = api.nvim_buf_line_count(bufnr)
-
-  local toprow
-  for i = curow, 1, -1 do
-    local insp = vim.fn.indent(i)
-    if insp == 0 then
-      if not cthulhu.nvim.is_empty_line(bufnr, i - 1) then break end
-      -- skip blank line
-    elseif insp < nsp then
-      break
-    else
-      toprow = i
-    end
-  end
-
-  local botrow
-  for i = curow, maxrow do
-    local insp = vim.fn.indent(i)
-    if insp == 0 then
-      if not cthulhu.nvim.is_empty_line(bufnr, i - 1) then break end
-      -- skip blank line
-    elseif insp < nsp then
-      break
-    else
-      botrow = i
-    end
-  end
+  local toprow = resolve_stop_row(bufnr, nsp, itertools.range(curow, 0 - 1, -1))
+  local botrow = resolve_stop_row(bufnr, nsp, itertools.range(curow, buflines.count(bufnr) + 1, 1))
 
   jelly.debug("curow=%d, nsp=%d, rowrange=%d-%d", curow, nsp, toprow, botrow)
   jumplist.push_here()
-  vsel.select_lines(toprow - 1, botrow - 1 + 1)
+  vsel.select_lines(winid, toprow - 1, botrow - 1 + 1)
 end
